a355c94b092d9d4dca262478a1279c6aa87fb1cb
Don <gcrushx2@gmail.com>
2016-11-25 04:32:25 +0000

    mobilefall2016

--- mobilefall2016/onebutton/script.js
+++ mobilefall2016/onebutton/script.js
@@ -1,19 +1,13 @@
-/*var btn = document.getElementById("btn");
+var btn = document.getElementById("btn");
 var msg = document.getElementById("msg");
+
+btn.addEventListener("mousedown", eventCallback);
+btn.addEventListener("mouseup", eventCallback);
+btn.addEventListener("mouseout", eventCallback);
+btn.addEventListener("mouseover", eventCallback);
+ 
 
 function eventCallback(e){
     msg.innerHTML = e.type;
 }
 
-btn.addEventListener("mousedown", eventCallback);
-btn.addEventListener("mouseup", eventCallback);
-btn.addEventListener("mouseout", eventCallback);
-btn.addEventListener("mouseover", eventCallback);*/
-function main(){
-    $('.msg').hide();
-    $('.btn').on('click', function(){
-        $('msg').toggle();
-    });
-}
-
-$(document).ready(main);
